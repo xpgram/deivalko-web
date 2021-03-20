9398b7ee3d14aaf3c65132a3922833c6ba772f56
Devin Biddy <xp.gram@gmail.com>
2021-03-20 11:23:56 -0700

    add post-landing section details; mostly experimental

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Background } from './components/Background';
 import Navbar from './components/header/Navbar';
+import Section from './components/Section';
 
 import './components/common.css';
 
@@ -8,15 +9,27 @@ function App() {
   return (
     <div className="App">
       <Router>
+
         <Navbar />
-        <Switch>
+        <Switch>          {/* What does Switch do? */}
           <Route path='/' exact />
         </Switch>
 
         <Background />
-        <div className="section-glow-upward">
-          <Background />
-        </div>
+        {/* Rename to landing or something; where should 'main' go? */}
+        {/* Also, make it a container that has the logo image in it. */}
+
+        <Section className="section-glow-upward">
+          <h1>Hey</h1>
+          <p style={{maxWidth: '30rem', margin: '0 auto 0 auto'}}>
+            My name is <span style={{color: '#969'}}>Dei</span> <span style={{color: '#A58'}}>Valko</span>.
+            I am an indie games designer</p>
+          <p style={{maxWidth: '50rem', margin: '0 auto 0 auto'}}>
+            and artist based in Oregon. Blah blah, more stuff, I like doing things that are cool.
+            Do something cool with me sometime.
+          </p>
+        </Section>
+
       </Router>
     </div>
   );
