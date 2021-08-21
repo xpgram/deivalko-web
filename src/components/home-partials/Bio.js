8a27194b3f62cfbc6f76ecbb3fe2a84606cadc9f
Devin Biddy <xp.gram@gmail.com>
2021-08-21 08:37:01 -0700

    update bio section

--- src/components/home-partials/Bio.js
+++ src/components/home-partials/Bio.js
@@ -1,19 +1,20 @@
 import { Component } from "react";
 
+import styles from './Bio.module.scss';
 
 export class Bio extends Component {
   render() {
     return (
-      <>
-        <h1>Hey</h1>
-        <p style={{maxWidth: '30rem', margin: '0 auto 0 auto'}}>
-          My name is <span style={{color: '#969'}}>Dei</span> <span style={{color: '#A58'}}>Valko</span>.
+      <div className={styles.wrapper}>
+        <h1 className={styles.attentionGrabber}>Hey</h1>
+        <p className={styles.text}>
+          My name is <span style={{color: '#A7A'}}>Dei</span> <span style={{color: '#C7A'}}>Valko</span>.
           I am an indie games designer</p>
-        <p style={{maxWidth: '50rem', margin: '0 auto 0 auto'}}>
+        <p className={styles.text}>
           and artist based in Oregon.
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error iusto delectus quod corrupti necessitatibus magnam perferendis voluptas voluptate totam enim animi rem neque nisi doloribus cumque quis, laborum cum at?
         </p>
-      </>
+      </div>
     )
   }
 }
