0dc5f47eeffe32b7f29bdfedc94b9958a0b4002e
Devin Biddy <xp.gram@gmail.com>
2021-08-24 00:38:28 -0700

    change portfolio presentation style: frosted glass

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -6,16 +6,16 @@ import demoImage from "../resources/img/PixilBG.png";
 export class PortfolioItem extends Component {
   render() {
     return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h2 className={styles.title}>{this.props.title}</h2>
-          <div className={styles.visualWrapper}>
-            <div className={styles.column} />
-            <img className={styles.visual} src={demoImage} alt="" />
+      // <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.frostedGlass}>
+            <div className={styles.visualWrapper}>
+              <img className={styles.visual} src={demoImage} alt="" />
+              <h2 className={styles.title}>{this.props.title}</h2>
+            </div>
           </div>
         </div>
-        <div className={styles.description}>{this.props.description}</div>
-      </div>
+      // </div>
     );
   }
 }
--- src/components/home-partials/Portfolio.js
+++ src/components/home-partials/Portfolio.js
@@ -3,11 +3,14 @@ import { PortfolioItem } from "../PortfolioItem";
 
 import styles from "./Portfolio.module.scss";
 
+import test from "../../resources/img/PixilBG.png";
+
 export class Portfolio extends Component {
   render() {
     return (
       <div className={styles.background}>
         <div className={styles.container}>
+          <img src={test} className={styles.image} />
           <PortfolioItem
             title="Advance Wars Clone using Pixi.js and my own brain"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos similique qui sunt exercitationem commodi sint illo velit ea voluptas, maxime aspernatur enim veniam, accusamus modi sed voluptatibus quia? Accusamus rem illo corporis dolorum recusandae excepturi reiciendis a omnis rerum qui."
