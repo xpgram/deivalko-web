ce0411ea2d44324a16adb0db09db83b0a7d52443
Devin Biddy <xp.gram@gmail.com>
2021-08-24 12:42:59 -0700

    adjust visual effects / add color demo image

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,21 +1,22 @@
 import { Component } from "react";
 
 import styles from "./PortfolioItem.module.scss";
-import demoImage from "../resources/img/PixilBG.png";
+
+import demoImage from "../resources/img/portfolio/advance-wars-clone.png";
 
 export class PortfolioItem extends Component {
+  
+
   render() {
     return (
-      // <div className={styles.wrapper}>
-        <div className={styles.container}>
-          <div className={styles.frostedGlass}>
-            <div className={styles.visualWrapper}>
-              <img className={styles.visual} src={demoImage} alt="" />
-              <h2 className={styles.title}>{this.props.title}</h2>
-            </div>
+      <div className={styles.container}>
+        <div className={styles.frostedGlass}>
+          <div className={styles.visualWrapper}>
+            <img className={styles.visual} src={demoImage} alt="" />
+            <h2 className={styles.title}>{this.props.title}</h2>
           </div>
         </div>
-      // </div>
+      </div>
     );
   }
 }
