9f01a1e1fa6617a0b89a25f12fa4406ec3f30fa3
Devin Biddy <xp.gram@gmail.com>
2021-08-23 20:26:14 -0700

    finish card-style portfolio items

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,20 +1,20 @@
 import { Component } from "react";
 
 import styles from "./PortfolioItem.module.scss";
-
+import demoImage from "../resources/img/PixilBG.png";
 
 export class PortfolioItem extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <h2 className={styles.title}>{this.props.title}</h2>
-        <div className={styles.contentWrapper}>
+        <div className={styles.header}>
+          <h2 className={styles.title}>{this.props.title}</h2>
           <div className={styles.visualWrapper}>
             <div className={styles.column} />
-            <img className={styles.visual} src="" alt="" />
+            <img className={styles.visual} src={demoImage} alt="" />
           </div>
-          <div className={styles.description}>{this.props.description}</div>
         </div>
+        <div className={styles.description}>{this.props.description}</div>
       </div>
     );
   }
--- src/components/home-partials/Portfolio.js
+++ src/components/home-partials/Portfolio.js
@@ -9,16 +9,16 @@ export class Portfolio extends Component {
       <div className={styles.background}>
         <div className={styles.container}>
           <PortfolioItem
-            title="Advance Wars Clone"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis gravida euismod. Phasellus nec consectetur magna. Ut condimentum, lectus et commodo sollicitudin, tortor nulla tempus nibh, id dapibus est ante sit amet nunc. Maecenas blandit tortor tellus, eget dapibus diam imperdiet vitae. Donec gravida lacus."
+            title="Advance Wars Clone using Pixi.js and my own brain"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos similique qui sunt exercitationem commodi sint illo velit ea voluptas, maxime aspernatur enim veniam, accusamus modi sed voluptatibus quia? Accusamus rem illo corporis dolorum recusandae excepturi reiciendis a omnis rerum qui."
           />
           <PortfolioItem
             title="Advance Wars Clone"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis gravida euismod. Phasellus nec consectetur magna. Ut condimentum, lectus et commodo sollicitudin, tortor nulla tempus nibh, id dapibus est ante sit amet nunc. Maecenas blandit tortor tellus, eget dapibus diam imperdiet vitae. Donec gravida lacus."
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque ab tempora, aspernatur amet, sequi maxime, velit voluptatum fugiat itaque consequatur nemo maiores sapiente quis."
           />
           <PortfolioItem
-            title="Advance Wars Clone"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis gravida euismod. Phasellus nec consectetur magna. Ut condimentum, lectus et commodo sollicitudin, tortor nulla tempus nibh, id dapibus est ante sit amet nunc. Maecenas blandit tortor tellus, eget dapibus diam imperdiet vitae. Donec gravida lacus."
+            title="Advance Wars Clone I Built Without My Dad's Permission"
+            description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident quod, sequi odit dolores corrupti quas animi rerum ex in, laudantium itaque eum labore nostrum voluptatem mollitia. Iusto omnis totam dolor ipsa cupiditate quaerat eius suscipit delectus assumenda distinctio! Ab velit necessitatibus assumenda iusto consectetur mollitia, similique molestias quo odio provident perferendis dicta, maiores quibusdam enim quasi, accusamus modi ad deserunt."
           />
         </div>
       </div>
