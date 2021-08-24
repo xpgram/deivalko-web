629439492e45d9969425c31c186797a9a9934b0d
Devin Biddy <xp.gram@gmail.com>
2021-08-24 15:29:28 -0700

    update footer design

--- src/components/header/Footer.js
+++ src/components/header/Footer.js
@@ -2,11 +2,18 @@ import { Component } from "react";
 
 import styles from "./Footer.module.scss";
 
+import background from "../../resources/img/PixilBG.png";
+
 export class Footer extends Component {
+
   render () {
+    const year = new Date().getFullYear().toString();
+
     return (
-      <div className={styles.background}>
-        <div className={styles.text}>DEI VALKO | 2021</div>
+      <div className={styles.background}
+        style={{backgroundImage: `url(${background})`}}
+      >
+        <div className={styles.text}>&copy; DEI VALKO &nbsp;|&nbsp; {year}</div>
       </div>
     );
   }
