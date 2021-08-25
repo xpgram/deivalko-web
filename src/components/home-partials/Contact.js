a6b334454bac066fb3e4081a1b1158191f38940d
Devin Biddy <xp.gram@gmail.com>
2021-08-24 23:51:07 -0700

    add contact section styles

--- src/components/home-partials/Contact.js
+++ src/components/home-partials/Contact.js
@@ -3,16 +3,42 @@ import { Component } from "react";
 import styles from "./Contact.module.scss";
 
 export class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {showMsg: false};
+  }
+
+  onSubmit = () => {
+    this.setState({showMsg: true});
+    setTimeout(() => {this.setState({showMsg: false})}, 3000);
+  }
+
   render() {
     return (
       <div className={styles.background}>
-        <h1 className={styles.title}>Contact</h1>
+        <h1 className={styles.header}>Contact</h1>
         <p className={styles.text}>
-          Tell me you love me <a>here</a> and maybe I'll tell you back.</p>
+          Tell me you love me and maybe I'll tell you back.
+        </p>
         <form>
-          <input placeholder={'Write your message here.'} />
-          <button type="submit">Submit</button>
+          <textarea
+            className={styles.input}
+            placeholder={'Write your message here.'}
+          />
+          <button
+            className={styles.button}
+            type="button"
+            onClick={this.onSubmit}
+          >
+            Submit
+          </button>
         </form>
+
+        <div className={`${styles.prankText} ${this.state.showMsg && styles.prankTextShow}`}>
+          Ha! Pranked you, bro. That button doesn't do anything.
+        </div>
+
       </div>
     )
   }
