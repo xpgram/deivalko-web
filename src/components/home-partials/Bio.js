ed1bf5e62771586266fffd6d49e4c0fe741e5c9c
Devin Biddy <xp.gram@gmail.com>
2021-08-24 23:49:30 -0700

    update about me text

--- src/components/home-partials/Bio.js
+++ src/components/home-partials/Bio.js
@@ -9,9 +9,13 @@ export class Bio extends Component {
         <h1 className={styles.attentionGrabber}>Hey</h1>
         <p className={styles.text}>
           My name is <span style={{color: '#A7A'}}>Dei</span> <span style={{color: '#C7A'}}>Valko</span>.
-          I am an indie games designer</p>
+          I am an indie games designer
+        </p>
         <p className={styles.text}>
-          and artist based in Oregon.
+          and artist based in Oregon. This site is still under construction, but I'm not getting any business right now anyway, so whatever.
+        </p>
+        <br/>
+        <p className={styles.text}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error iusto delectus quod corrupti necessitatibus magnam perferendis voluptas voluptate totam enim animi rem neque nisi doloribus cumque quis, laborum cum at?
         </p>
       </div>
